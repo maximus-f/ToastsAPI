ef99357bc48a130d82d22a135aaa69341b22fdf2
perotins <maxfuligni@gmail.com>
2025-06-04 23:07:59 -0400

    add 1.20.3

--- NMS/1_20_R3/src/main/java/com/frengor/toastapi/nms/v1_20_R3/packets/PacketPlayOutAdvancementsWrapper_v1_20_R3.java
+++ NMS/1_20_R3/src/main/java/com/frengor/toastapi/nms/v1_20_R3/packets/PacketPlayOutAdvancementsWrapper_v1_20_R3.java
@@ -6,8 +6,9 @@ import com.frengor.toastapi.nms.wrappers.MinecraftKeyWrapper;
 import com.frengor.toastapi.nms.wrappers.advancement.AdvancementWrapper;
 import com.frengor.toastapi.nms.wrappers.packets.PacketPlayOutAdvancementsWrapper;
 import com.google.common.collect.Maps;
-import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket;
 import net.minecraft.resources.ResourceLocation;
 import org.bukkit.entity.Player;
@@ -16,7 +17,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class PacketPlayOutAdvancementsWrapper_v1_20_R3 extends PacketPlayOutAdvancementsWrapper {
@@ -27,27 +27,20 @@ public class PacketPlayOutAdvancementsWrapper_v1_20_R3 extends PacketPlayOutAdva
         this.packet = new ClientboundUpdateAdvancementsPacket(true, Collections.emptyList(), Collections.emptySet(), Collections.emptyMap());
     }
 
-    @SuppressWarnings("unchecked")
     public PacketPlayOutAdvancementsWrapper_v1_20_R3(@NotNull Map<AdvancementWrapper, Integer> toSend) {
         Map<ResourceLocation, AdvancementProgress> map = Maps.newHashMapWithExpectedSize(toSend.size());
-        for (Entry<AdvancementWrapper, Integer> e : toSend.entrySet()) {
+        for (Map.Entry<AdvancementWrapper, Integer> e : toSend.entrySet()) {
             AdvancementWrapper adv = e.getKey();
-            // For toast notifications, we just need a simple progress
-            AdvancementProgress progress = new AdvancementProgress();
-            progress.update(((Advancement) adv.toNMS()).getCriteria(), ((Advancement) adv.toNMS()).getRequirements());
-            progress.getCriterion("0").grant();
-            map.put((ResourceLocation) adv.getKey().toNMS(), progress);
+            map.put((ResourceLocation) adv.getKey().toNMS(), Util.getAdvancementProgress((AdvancementHolder) adv.toNMS(), e.getValue().intValue()));
         }
-        this.packet = new ClientboundUpdateAdvancementsPacket(false, (Collection<Advancement>) ListSet.fromWrapperSet(toSend.keySet()), Collections.emptySet(), map);
+        this.packet = new ClientboundUpdateAdvancementsPacket(false, (Collection) ListSet.fromWrapperSet(toSend.keySet()), Collections.emptySet(), map);
     }
 
-    @SuppressWarnings("unchecked")
     public PacketPlayOutAdvancementsWrapper_v1_20_R3(@NotNull Set<MinecraftKeyWrapper> toRemove) {
-        this.packet = new ClientboundUpdateAdvancementsPacket(false, Collections.emptyList(), (Set<ResourceLocation>) ListSet.fromWrapperSet(toRemove), Collections.emptyMap());
+        this.packet = new ClientboundUpdateAdvancementsPacket(false, Collections.emptyList(), (Set) ListSet.fromWrapperSet(toRemove), Collections.emptyMap());
     }
 
-    @Override
     public void sendTo(@NotNull Player player) {
-        Util.sendTo(player, packet);
+        Util.sendTo(player, (Packet) this.packet);
     }
 }
